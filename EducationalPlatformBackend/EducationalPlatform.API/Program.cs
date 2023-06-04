995ea17c864604f183618d37d09f8e7e2266a285
Piotr Gębalski <piotrgebalski5@gmail.com>
2023-06-04 22:00:57 +0200

    Add Serilog

--- EducationalPlatformBackend/EducationalPlatform.API/Program.cs
+++ EducationalPlatformBackend/EducationalPlatform.API/Program.cs
@@ -1,6 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
+builder.Services.AddControllers();
+
+builder.Host.UseSerilog((context, configuration) => 
+    configuration.ReadFrom.Configuration(context.Configuration));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapGet("/", ([FromServices] ILogger<Program> loggerr) =>
+{
+    loggerr.LogInformation("Hello World {@Now}", DateTime.Now);
+
+    return "Hello world";
+});
 
 app.Run();
