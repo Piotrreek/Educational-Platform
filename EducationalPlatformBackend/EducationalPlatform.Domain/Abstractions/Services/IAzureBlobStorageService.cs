ca8f1efe2c739de24d5895600e7ce0c3e3b5edd5
Piotr Gębalski <piotrgebalski5@gmail.com>
2023-07-01 23:52:07 +0200

    Add implementations for uploading and getting blob file

--- EducationalPlatformBackend/EducationalPlatform.Domain/Abstractions/Services/IAzureBlobStorageService.cs
+++ EducationalPlatformBackend/EducationalPlatform.Domain/Abstractions/Services/IAzureBlobStorageService.cs
@@ -1,6 +1,10 @@
+using EducationalPlatform.Domain.Models;
+using Microsoft.AspNetCore.Http;
+
 namespace EducationalPlatform.Domain.Abstractions.Services;
 
 public interface IAzureBlobStorageService
 {
-    
+    Task<BlobDto> GetBlobByNameAsync(Guid blobName);
+    Task UploadBlobAsync(Guid blobName, IFormFile file);
 }
