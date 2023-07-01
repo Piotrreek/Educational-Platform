cc022a019a6d10aa28c803615bd6a2f4ec918cd8
Piotr Gębalski <piotrgebalski5@gmail.com>
2023-07-01 23:06:46 +0200

    Create empty service and register in DI container

---
+++ EducationalPlatformBackend/EducationalPlatform.Domain/Abstractions/Services/IAzureBlobStorageService.cs
@@ -0,0 +1,6 @@
+namespace EducationalPlatform.Domain.Abstractions.Services;
+
+public class IAzureBlobStorageService
+{
+    
+}
---
+++ EducationalPlatformBackend/EducationalPlatform.Infrastructure/Services/AzureBlobStorageService.cs
@@ -0,0 +1,8 @@
+using EducationalPlatform.Domain.Abstractions.Services;
+
+namespace EducationPlatform.Infrastructure.Services;
+
+public class AzureBlobStorageService : IAzureBlobStorageService
+{
+    
+}
