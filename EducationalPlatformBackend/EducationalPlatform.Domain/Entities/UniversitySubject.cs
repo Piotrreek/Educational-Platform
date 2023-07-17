a895a4ebdce394a83730aeeced2a590a01ad7c85
Piotr Gębalski <91609048+Piotrreek@users.noreply.github.com>
2023-07-17 21:21:14 +0200

    Extract common logic of domain to lower level (#11)

--- EducationalPlatformBackend/EducationalPlatform.Domain/Entities/UniversitySubject.cs
+++ EducationalPlatformBackend/EducationalPlatform.Domain/Entities/UniversitySubject.cs
@@ -9,16 +9,14 @@ namespace EducationalPlatform.Domain.Entities;
 
 public class UniversitySubject : AcademyEntity
 {
-    public string Name { get; private set; } = null!;
     public UniversitySubjectDegree UniversitySubjectDegree { get; private set; }
     public Faculty Faculty { get; private set; } = null!;
     public Guid FacultyId { get; private set; }
     private readonly List<UniversityCourse> _universityCourses = new();
     public IReadOnlyCollection<UniversityCourse> UniversityCourses => _universityCourses;
 
-    internal UniversitySubject(string name, UniversitySubjectDegree universitySubjectDegree)
+    internal UniversitySubject(string name, UniversitySubjectDegree universitySubjectDegree) : base(name)
     {
-        Name = name;
         UniversitySubjectDegree = universitySubjectDegree;
     }
 
--- EducationalPlatformBackend/EducationalPlatform.Domain/Primitives/AcademyEntity.cs
+++ EducationalPlatformBackend/EducationalPlatform.Domain/Primitives/AcademyEntity.cs
@@ -4,6 +4,16 @@ namespace EducationalPlatform.Domain.Primitives;
 
 public abstract class AcademyEntity : Entity
 {
+    public string Name { get; private set; } = null!;
     private readonly List<User> _users = new();
     public IReadOnlyCollection<User> Users => _users;
+
+    protected AcademyEntity(string name)
+    {
+        Name = name;
+    }
+
+    protected AcademyEntity()
+    {
+    }
 }
