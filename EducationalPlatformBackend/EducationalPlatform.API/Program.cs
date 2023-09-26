f701bf56f30f405faf915dd0d1030d01c79af7e5
Piotr Gębalski <piotrgebalski5@gmail.com>
2023-09-26 13:44:04 +0200

    update cors

--- EducationalPlatformBackend/EducationalPlatform.API/Program.cs
+++ EducationalPlatformBackend/EducationalPlatform.API/Program.cs
@@ -64,7 +64,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: "Baza",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000")
+            policy.WithOrigins(builder.Configuration["ApplicationUrl"]!)
                 .AllowAnyHeader()
                 .AllowAnyMethod()
                 .AllowCredentials();
